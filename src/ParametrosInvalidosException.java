b1b9a92dccfdbe1c322b330cb68171606855ad83
Tarita Lima <tarita36@hotmail.com>
2025-07-23 17:42:46 -0300

    Desafio controle de fluxo

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,12 @@
+
+public class ParametrosInvalidosException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(final String message) {
+		super(message);
+	}	 
+}
